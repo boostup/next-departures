deb850a0e8678ce989054a53474d4f239e2fbfad
Fred B. <boostup@gmail.com>
2026-06-29 09:21:52 +0200

    Updates

--- src/icons/icon-arrow-right.js
+++
@@ -1,9 +0,0 @@
-/**
- * Arrow right / forward icon.
- */
-export function iconArrowRight({ size = 24, className = '' } = {}) {
-    return `<svg width="${size}" height="${size}" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="${className}">
-    <line x1="5" y1="12" x2="19" y2="12"/>
-    <polyline points="12 5 19 12 12 19"/>
-  </svg>`;
-}
